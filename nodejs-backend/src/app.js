9659955a418874153839312c9f500d3c94fd7176
HenryPeralta <henrygabrielperalta@gmail.com>
2025-04-23 21:52:59 -0600

    Agregando ruta para el balanceador

--- nodejs-backend/src/app.js
+++ nodejs-backend/src/app.js
@@ -33,6 +33,10 @@ app.use('/task', taskRoutes)
 // Files
 app.use('/file', fileRoutes)
 
+app.get('/health', (req, res) => {
+  res.status(200).send('OK')
+})
+
 // Manejo de errores 404
 app.use((_req, res, next) => {
   res.status(404).json({ message: 'Recurso no encontrado' })
